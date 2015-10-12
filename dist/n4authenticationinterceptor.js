b3c87b2097457f6eb998a6789fd5d8c00aab77ec
Tiago Resende <tiagor87@gmail.com>
2015-10-12 17:03:00 -0300

    Ajuste para minificação.

--- dist/n4authenticationinterceptor.js
+++ dist/n4authenticationinterceptor.js
@@ -1,22 +1,22 @@
-"use strict";
-
 ;
 (function(ng) {
+  "use strict";
+
   ng
-    .module('n4AuthenticationInterceptor', [])
-    .config(['$httpProvider', function($httpProvider) {
-      $httpProvider.interceptors.unshift('n4AuthenticationInterceptor');
+    .module("n4AuthenticationInterceptor", [])
+    .config(["$httpProvider", function($httpProvider) {
+      $httpProvider.interceptors.unshift("n4AuthenticationInterceptor");
     }])
-    .provider('n4AuthenticationInterceptor', [
+    .provider("n4AuthenticationInterceptor", [
       function() {
         var self = this;
 
         self.cb = angular.noop; //callback to be executed before the redirect
         self.redirectURL = undefined;
-        self.notAuthenticatedMessage = 'Usuário não autenticado.';
+        self.notAuthenticatedMessage = "Usuário não autenticado.";
         self.statusHttp = 401;
 
-        self.$get = ['$q', '$window', '$log', function($q, $window, $log) {
+        self.$get = ["$q", "$window", "$log", function($q, $window, $log) {
           return {
             responseError: function(rejection) {
               if (rejection.status === self.statusHttp) {
@@ -32,4 +32,4 @@
       }
     ]);
 
-}(angular))
+}(angular));
